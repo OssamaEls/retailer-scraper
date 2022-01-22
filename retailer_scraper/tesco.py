a043224db4ef75e4c6ab52246c6983ef946ec5d8
Ossama <elsossama@gmail.com>
2022-01-22 12:55:35 +0000

    BeautifulSoup instead of Selenium

--- retailer_scraper/tesco.py
+++ retailer_scraper/tesco.py
@@ -1,6 +1,21 @@
-from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
-
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
 
 
 class TescoScraper:
-    pass
+    base_url = 'https://www.tesco.com/groceries/en-GB/search?query='
+
+    def __init__(self, query: str):
+        self.query = query
+        self._results = None
+
+    def run_query(self):
+        url = self.base_url + quote(self.query)
+        first_page = requests.get(url)
+        first_page_html = first_page.text
+        soup = BeautifulSoup(first_page_html, 'html.parser')
+        return soup
+
+
+
