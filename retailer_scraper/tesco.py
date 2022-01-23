984d2dc9006c3ffa7401efabbda48e4275d7e8b1
Ossama <elsossama@gmail.com>
2022-01-23 14:26:47 +0000

    Add header with user-agent

--- retailer_scraper/tesco.py
+++ retailer_scraper/tesco.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 
+from headers import headers
 
 class TescoScraper:
     base_url = 'https://www.tesco.com/groceries/en-GB/search?query='
@@ -12,10 +13,17 @@ class TescoScraper:
 
     def run_query(self):
         url = self.base_url + quote(self.query)
-        first_page = requests.get(url)
-        first_page_html = first_page.text
-        soup = BeautifulSoup(first_page_html, 'html.parser')
-        return soup
+        output = requests.get(url, headers=headers, timeout=5)
+        first_page = output.text
+        results = BeautifulSoup(first_page, 'html.parser')
+        return results
+
+    @property
+    def found_exact_match(self):
+        assert self._results is not None, 'You have not run any query yet.'
 
 
 
+tesco_scraper = TescoScraper('kfdsgfbskdhbd')
+soup = tesco_scraper.run_query()
+result = soup.find(name='p', attrs={'class': 'results-title'})
