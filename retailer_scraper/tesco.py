7c9b3f1fa4b5fa794beb729ff2228da36163ba0e
Ossama <elsossama@gmail.com>
2022-01-23 14:56:41 +0000

    Count number of items found

--- retailer_scraper/tesco.py
+++ retailer_scraper/tesco.py
@@ -4,6 +4,7 @@ from urllib.parse import quote
 
 from headers import headers
 
+
 class TescoScraper:
     base_url = 'https://www.tesco.com/groceries/en-GB/search?query='
 
@@ -15,8 +16,22 @@ class TescoScraper:
         url = self.base_url + quote(self.query)
         output = requests.get(url, headers=headers, timeout=5)
         first_page = output.text
-        results = BeautifulSoup(first_page, 'html.parser')
-        return results
+        soup = BeautifulSoup(first_page, 'html.parser')
+        found_items = soup.find(name='div', attrs={'class': 'product-list-container'})
+        if not found_items:
+            self._results = []
+            return
+        # second <strong> tag under the div tag with class 'pagination__items-displayed' has text: 'xxx items'
+        number_of_items = soup.find(
+            name='div',
+            attrs={'class': 'pagination__items-displayed'}
+        ).findChildren(
+            name='strong'
+        )[
+            1
+        ].text.split()[0]
+        number_of_pages = number_of_items // 50
+        return soup
 
     @property
     def found_exact_match(self):
@@ -24,6 +39,7 @@ class TescoScraper:
 
 
 
-tesco_scraper = TescoScraper('kfdsgfbskdhbd')
+tesco_scraper = TescoScraper('free range eggs')
 soup = tesco_scraper.run_query()
-result = soup.find(name='p', attrs={'class': 'results-title'})
+# result = soup.find(name='p', attrs={'class': 'results-title'})
+result = soup.find(name='div', attrs={'class': 'pagination__items-displayed'})
