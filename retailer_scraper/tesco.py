ecf9147182e0189c609d70a360a21119d9a6abfe
Ossama <elsossama@gmail.com>
2022-01-26 22:14:42 +0000

    Parse all page results and get individual links

--- retailer_scraper/tesco.py
+++ retailer_scraper/tesco.py
@@ -2,28 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 from math import ceil
+from typing import List
 
 from headers import headers
 
 
 class TescoScraper:
     base_url = 'https://www.tesco.com/groceries/en-GB/search?query='
+    num_items_per_page = 48
 
     def __init__(self, query: str):
         self.query = query
-        self._results = None
+        self._items = []
+
+    def get_url(self, page_number: int):
+        return f'{self.base_url}{quote(self.query)}&page={page_number}&count={self.num_items_per_page}'
 
     def run_query(self):
-        url = self.base_url + quote(self.query)
-        output = requests.get(url, headers=headers, timeout=5)
-        first_page = output.text
-        soup = BeautifulSoup(first_page, 'html.parser')
+        soup = self.parse_page(1)
         found_items = soup.find(name='div', attrs={'class': 'product-list-container'})
         if not found_items:
-            self._results = []
             return
         # second <strong> tag under the div tag with class 'pagination__items-displayed' has text: 'xxx items'
-        number_of_items = soup.find(
+        num_items = soup.find(
             name='div',
             attrs={'class': 'pagination__items-displayed'}
         ).findChildren(
@@ -31,25 +32,41 @@ class TescoScraper:
         )[
             1
         ].text.split()[0]
-        number_of_pages = ceil(int(number_of_items)/50)
-        # for page in number_of_pages:
-        #     item = soup.find(
-        #         name='li',
-        #         attrs={'class': 'product-list--list-item'}
-        #     )
-
-        return soup
+        number_of_pages = ceil(int(num_items)/self.num_items_per_page)
+        for page_number in range(1, number_of_pages + 1):
+            if page_number != 1:
+                soup = self.parse_page(page_number)
+            self._items += soup.find_all(
+                name='li',
+                attrs={'class': 'product-list--list-item'}
+            )[:self.num_items_per_page]
 
     @property
     def found_exact_match(self):
-        assert self._results is not None, 'You have not run any query yet.'
+        assert self._items is not None, 'You have not run any query yet.'
 
+    @property
+    def items(self):
+        return self._items
+
+    def individual_page_links(self) -> List[str]:
+        return [
+            item.find(
+                name='a',
+                href=True
+            )['href']
+            for item in self.items
+        ]
+
+    def parse_page(self, page_number: int) -> BeautifulSoup:
+        url = self.get_url(page_number)
+        output = requests.get(url, headers=headers, timeout=5)
+        page = output.text
+        return BeautifulSoup(page, 'html.parser')
 
 
 tesco_scraper = TescoScraper('eggs')
-soup = tesco_scraper.run_query()
-# result = soup.find(name='p', attrs={'class': 'results-title'})
-result = soup.find_all(
-                name='li',
-                attrs={'class': 'product-list--list-item'}
-            )
+tesco_scraper.run_query()
+items = tesco_scraper.items
+links = tesco_scraper.individual_page_links()
+
