ba5b20a2ef60fd423aecef5bd32f3dfb1d11025c
Ossama <elsossama@gmail.com>
2022-01-31 21:27:33 +0000

    Complete tests for separate Item class

--- retailer_scraper/item.py
+++ retailer_scraper/item.py
@@ -52,3 +52,9 @@ class Item:
 
     def __str__(self):
         return self.name
+
+    def __eq__(self, other):
+        return self.details == other.details
+
+    def __hash__(self):
+        return hash(self.name)
